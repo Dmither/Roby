ea294d2c04b36b0f02a729280691a9d1e46e59be
Dmither <mr.d.graumann@gmail.com>
2022-10-28 17:06:48 +0300

    add header

--- js/script.js
+++ js/script.js
@@ -1,7 +1,18 @@
 let burger = document.querySelector(".burger");
-let menu = document.querySelector(".header__main");
+let menu = document.querySelector(".header__menu");
+let body = document.querySelector("body");
+let logo = document.querySelector(".logo");
 
 burger.addEventListener("click", function(){
   burger.classList.toggle("burger_active");
-  menu.classList.toggle("main_active")
+  menu.classList.toggle("menu_active")
+  body.classList.toggle("freeze");
+})
+
+body.addEventListener('scroll', function(){
+  if (body.scrollTop > 40){
+    logo.classList.add('logo_scrolled');
+  } else {
+    logo.classList.remove('logo_scrolled');
+  }
 })
